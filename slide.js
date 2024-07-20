731092336d559beffc81ffda7ad4bfe6224b10cf
daniel <51289681+danielh13210@users.noreply.github.com>
2024-07-20 16:58:06 +0800

    Add files via upload

---
+++ slide.js
@@ -0,0 +1,10 @@
+function hide_all() {
+    var sections = document.getElementsByClassName("section");
+    for (let index = 0; index < sections.length; index++) {
+        const element = sections.item(index);
+        element.style.display = "none";
+    }
+}
+function switch_to(slide_id) {
+    location.replace(slide_id+".html");
+}
